7f451e6456ff8fb88c6f577a20e7d27330093316
Koo-sangwoo <luckytkddn@gmail.com>
2023-11-30 11:01:18 +0900

    [webview library setting complete]

---
+++ lib/screen/eform_webView_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class EFormWebScreen extends StatefulWidget {
+  const EFormWebScreen({super.key});
+
+  @override
+  State<EFormWebScreen> createState() => _EFormWebScreenState();
+}
+
+class _EFormWebScreenState extends State<EFormWebScreen> {
+
+  late InAppWebViewController _webViewController;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
